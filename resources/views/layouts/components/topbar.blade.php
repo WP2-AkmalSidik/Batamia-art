a6c93e221c4db7310fbd4963332b5db11ebd639c
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-06-04 21:02:03 +0700

    feat(admin): menambahkan layout dan style utama untuk semua tampilan

---
+++ resources/views/layouts/components/topbar.blade.php
@@ -0,0 +1,35 @@
+<header class="header-bg">
+    <div class="flex items-center justify-between px-6 py-4">
+        <div class="flex items-center gap-3">
+            <!-- Mobile menu button -->
+            <button onclick="toggleSidebar()"
+                class="lg:hidden text-gray-600 hover:text-gray-800 focus:outline-none">
+                <i class="fas fa-bars text-2xl"></i>
+            </button>
+
+            <h2 class="text-2xl font-bold">@yield('title')</h2>
+        </div>
+
+        <div class="flex items-center space-x-3">
+
+            <!-- Is Admin -->
+            <button class="relative ml-2">
+                <i class="fas fa-bell text-gray-600 hover:text-gray-400"></i>
+                <span
+                    class="absolute -top-2 -right-3 w-5 h-5 bg-gradient-to-r from-red-500 to-red-600 text-xs flex items-center justify-center text-white rounded-full animate-pulse">3</span>
+            </button>
+
+            <!-- Is Customers -->
+            <button class="relative ml-2">
+                <i class="fa-solid fa-cart-shopping text-gray-600 hover:text-gray-400"></i>
+                <span
+                    class="absolute -top-2 -right-3 w-5 h-5 bg-gradient-to-r from-red-500 to-red-600 text-xs flex items-center justify-center text-white rounded-full animate-pulse">3</span>
+            </button>
+
+            <!-- Theme Toggle (Icon only) -->
+            <button onclick="toggleTheme()" class="relative ml-4">
+                <i id="theme-icon" class="fas fa-moon text-5xl text-gray-600 hover:text-gray-400"></i>
+            </button>
+        </div>
+    </div>
+</header>
