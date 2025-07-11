1508486b7f78442662e29157a40369826059e940
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-07-11 17:43:17 +0700

    feat(guest): menambahkan halaman landing page untuk pengunjung

--- routes/web.php
+++ routes/web.php
@@ -88,3 +88,6 @@ Route::get('/produk', [UserDashboardController::class, 'index'])->name('user.pro
 Route::get('/produk/{id}/detail', [UserDashboardController::class, 'show'])->name('user.produk.detail');
 Route::get('/produk/{id}', [UserDashboardController::class, 'detailProduk'])->name('user.detail-produk');
 Route::view('/list-pesanan', 'pages/user/list-pesanan');
+
+// Guest
+Route::view('/beranda', 'guest/landing-page');
